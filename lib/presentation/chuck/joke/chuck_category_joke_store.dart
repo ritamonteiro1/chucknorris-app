93cec87617c789b99bc1d78b1b92f06ee9fdffe1
ritamonteiro1 <ritamonteiro@tokenlab.com.br>
2022-02-08 01:22:48 -0300

    create ChuckRandomJokeScreen

--- lib/presentation/chuck/joke/chuck_category_joke_store.dart
+++ lib/presentation/chuck/joke/chuck_category_joke_store.dart
@@ -1,3 +1,4 @@
+import '../../../domain/use_case/get_chuck_category_joke_use_case.dart';
 import 'package:mobx/mobx.dart';
 
 part 'chuck_category_joke_store.g.dart';
@@ -5,4 +6,13 @@ part 'chuck_category_joke_store.g.dart';
 class ChuckCategoryJokeStore = _ChuckCategoryJokeStore
     with _$ChuckCategoryJokeStore;
 
-abstract class _ChuckCategoryJokeStore with Store {}
+abstract class _ChuckCategoryJokeStore with Store {
+  _ChuckCategoryJokeStore(
+    this.getChuckCategoryJokeUseCase,
+  );
+
+  final GetChuckCategoryJokeUseCase getChuckCategoryJokeUseCase;
+
+  @observable
+  String name = '';
+}
--- lib/presentation/chuck/joke/chuck_random_joke_screen.dart
+++ lib/presentation/chuck/joke/chuck_random_joke_screen.dart
@@ -1,3 +1,11 @@
+import 'package:chuck_norris_app/data/remote/data_source/chuck_remote_data_source.dart';
+import 'package:chuck_norris_app/data/remote/data_source/chuck_remote_data_source_impl.dart';
+import 'package:chuck_norris_app/domain/repository/chuck_repository.dart';
+import 'package:chuck_norris_app/domain/repository/chuck_repository_impl.dart';
+import 'package:chuck_norris_app/domain/use_case/get_chuck_random_joke_use_case.dart';
+import 'package:chuck_norris_app/domain/use_case/get_chuck_random_joke_use_case_impl.dart';
+import 'package:dio/dio.dart';
+
 import '../../../constants/constant_images.dart';
 import '../../../generated/l10n.dart';
 import 'package:flutter/material.dart';
@@ -10,6 +18,18 @@ class ChuckRandomJokeScreen extends StatefulWidget {
 }
 
 class _ChuckRandomJokeScreenState extends State<ChuckRandomJokeScreen> {
+  late ChuckRemoteDataSource chuckRemoteDataSource;
+  late ChuckRepository chuckRepository;
+  late GetChuckRandomJokeUseCase getChuckRandomJokeUseCase;
+
+  @override
+  void initState() {
+    super.initState();
+    chuckRemoteDataSource = ChuckRemoteDataSourceImpl(Dio());
+    chuckRepository = ChuckRepositoryImpl(chuckRemoteDataSource);
+    getChuckRandomJokeUseCase = GetChuckRandomJokeUseCaseImpl(chuckRepository);
+  }
+
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
