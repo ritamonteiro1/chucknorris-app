54b72a72b25616106184a888b454e70027be6de0
ritamonteiro1 <ritamonteiro@tokenlab.com.br>
2022-02-08 01:24:32 -0300

    create ChuckCategoryJokeScreen

--- lib/presentation/chuck/joke/chuck_random_joke_store.dart
+++ lib/presentation/chuck/joke/chuck_random_joke_store.dart
@@ -1,7 +1,14 @@
+import '../../../domain/use_case/get_chuck_random_joke_use_case.dart';
 import 'package:mobx/mobx.dart';
 
 part 'chuck_random_joke_store.g.dart';
 
 class ChuckRandomJokeStore = _ChuckRandomJokeStore with _$ChuckRandomJokeStore;
 
-abstract class _ChuckRandomJokeStore with Store {}
+abstract class _ChuckRandomJokeStore with Store {
+  _ChuckRandomJokeStore(
+    this.getChuckRandomJokeUseCase,
+  );
+
+  final GetChuckRandomJokeUseCase getChuckRandomJokeUseCase;
+}
